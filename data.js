3f9be816c5077366747d9018dd0e549f4058b8f4
Borui Wang <borui@stanford.edu>
2013-03-04 23:23:46 -0800

    init

--- data.js
+++ data.js
@@ -1,19 +1,65 @@
+var log_data = [];
+
 var sample_color_map = {
-  0:"#113F8C",
-  1:"#01A4A4",
-  2:"#00A1CB",
-  3:"#61AE24",
-  4:"#D0D102",
-  5:"#32742C",
-  6:"#D70060",
-  7:"#61AE24",
-  8:"#E54028",
-  9:"#F18D05",
-  10:"#616161"
+  0:"#F5EDF4",
+  1:"#C6BAD5",
+  2:"#8F8DB4",
+  3:"#A39ABC",
+  4:"#BCDD7F",
+  5:"#6A4061",
+  6:"#65C9FF"
 };
+
+var raw_data = [
+{title: "24 Hour Party People.mp3",
+duration:"4:38",
+segment: "0:17_1 0:48_2 1:33_1 2:16_2 2:34_5 2:53_1 3:31_5 3:46_6 3:55_5 4:05_2"},
+{title: "Bryn Vampire Weekend.mp3",
+duration:"2:13",
+segment: "0:23_1 0:40_2 0:55_5 1:11_1 1:27_2 1:42_5 1:59_6"},
+{title: "Civilization Justice.mp3",
+duration:"4:11",
+segment: "0:22_1 1:05_2 2:49_6 3:15_1 3:32_2"},
+{title: "FIrst Aid.mp3",
+duration:"6:11",
+segment: "1:00_1 1:30_2 1:59_4 2:30_1 3:02_2 4:00_4 04:33_1"},
+{title: "Half Mast.mp3",
+duration:"3:54",
+segment: "0:21_1 0:37_2 0:53_1 1:22_2 1:37_5 1:52_1 2:22_2 2:52_1 3:23_5"},
+{title: "I Can Change LCD Soundsystem.mp3",
+duration:"5:55",
+segment: "0:25_1 1:32_2 2:14_1 3:17_2 4:09_1 4:40_2"}
+// {title: "It's War Blackbird Blackbird.mp3",
+// duration:"7:09",
+// segment: "2:01_1 2:18_5 2:26_2 2:35_5 2:51_2 4:03_5 4:36_2 5:09_5 5:24_2 5:58_5 6:13_2 6:37_5"},
+// {title: "Padding Out Mike Snow.mp3",
+// duration:"3:38",
+// segment: "0:15_1 0:44_2 0:59_1 1:45_2 2:15_5 2:49_2 3:18_6"},
+// {title: "Punching In A Dream.mp3",
+// duration:"3:34",
+// segment: "0:25_1 0:41_2 0:58_5 1:16_1 1:30_2 1:49_5 2:14_4 2:45_2 3:21_5"},
+// {title: "Send And Receive.mp3",
+// duration:"4:53",
+// segment: "1:16_1 2:11_2 3:22_1 3:48_2 4:19_1"},
+// {title: "Seven Nation Army.mp3",
+// duration:"3:51",
+// segment: "0:15_1 0:47_2 1:12_1 1:58_2 2:03_5 2:51_1 3:23_2 3:29_5"},
+// {title: "So Sorry Girl.mp3",
+// duration:"2:17",
+// segment: "0:00_1 1:00_2 1:27_1 2:06_5"},
+// {title: "Song For No One Mike Snow.mp3",
+// duration:"4:10",
+// segment: "0:16_1 0:50_2 1:07_6 1:32_1 2:05_2 2:23_6"},
+// {title: "Time To Pretend MGMT.mp3",
+// duration:"4:25",
+// segment: "0:31_1 1:09_2 1:45_1 2:15_6 2:34_1 2:53_2 3:59_6"},
+// {title: "All Blackbird Blackbird.mp3",
+// duration:"5:06",
+// segment: "0:14_1 1:49_5 2:35_1 3:55_6"}
+];
+
 var sample_seg_data = [];
-// would it be better to generate a number
-for(var i=0;i<20;i++){
+for(var i=0;i<5;i++){
   var segs = [];
   var progress = 0;
   for(var j=0;j<ri(3,6);j++){
@@ -33,101 +79,23 @@ for(var i=0;i<20;i++){
     segments:segs
   });
 }
-// building the segment bars using an array
+
+function build_seg_bars(){
+  for(var i=0;i<sample_seg_data.length;i++){
+    var segments = sample_seg_data[i].segments;
+    var d = sample_seg_data[i].duration;
+    var seg_bar = "<div class='seg_bar'><div class='sound_title'>Sound Title</div>";
+    var pt = 0;
+    for(var j=0; j<segments.length;j++ ){
+      seg_bar += "<div class='seg_part' style='width:"+100*((segments[j].time - pt)/d)+"%;background:"+sample_color_map[segments[j].type]+"'></div>";
+      pt = segments[j].time;
+    }
+    seg_bar += "</div>"
+    $("#seg_area").append(seg_bar);
+  }
+}
+
 var sample_seg_data_template = [
-  {
-    meta:{},
-    duration: 240,
-    segments: [
-      {
-        info: "",
-        time:43,
-        type:0
-      },
-      {
-        info: "",
-        time:90,
-        type:4
-      },
-      {
-        info: "",
-        time:130,
-        type:3
-      },
-      {
-        info: "",
-        time:200,
-        type:9
-      },
-      {
-        info: "",
-        time:240,
-        type:8
-      }
-    ]
-  },
-  {
-    meta:{},
-    duration: 240,
-    segments: [
-      {
-        info: "",
-        time:43,
-        type:0
-      },
-      {
-        info: "",
-        time:90,
-        type:4
-      },
-      {
-        info: "",
-        time:130,
-        type:3
-      },
-      {
-        info: "",
-        time:200,
-        type:9
-      },
-      {
-        info: "",
-        time:240,
-        type:8
-      }
-    ]
-  },
-  {
-    meta:{},
-    duration: 240,
-    segments: [
-      {
-        info: "",
-        time:43,
-        type:0
-      },
-      {
-        info: "",
-        time:90,
-        type:4
-      },
-      {
-        info: "",
-        time:130,
-        type:3
-      },
-      {
-        info: "",
-        time:200,
-        type:9
-      },
-      {
-        info: "",
-        time:240,
-        type:8
-      }
-    ]
-  },
   {
     meta:{},
     duration: 240,
@@ -161,8 +129,150 @@ var sample_seg_data_template = [
   }
 ];
 
+// get random integer between min and max (inclusive)
 function ri(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+*
+*  Base64 encode / decode
+*  http://www.webtoolkit.info/
+*
+**/
+var Base64 = {
+
+  // private property
+  _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+  // public method for encoding
+  encode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = Base64._utf8_encode(input);
+
+      while (i < input.length) {
+
+          chr1 = input.charCodeAt(i++);
+          chr2 = input.charCodeAt(i++);
+          chr3 = input.charCodeAt(i++);
+
+          enc1 = chr1 >> 2;
+          enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+          enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+          enc4 = chr3 & 63;
+
+          if (isNaN(chr2)) {
+              enc3 = enc4 = 64;
+          } else if (isNaN(chr3)) {
+              enc4 = 64;
+          }
+
+          output = output +
+          this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+          this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+      }
+
+      return output;
+  },
+
+  // public method for decoding
+  decode : function (input) {
+      var output = "";
+      var chr1, chr2, chr3;
+      var enc1, enc2, enc3, enc4;
+      var i = 0;
+
+      input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+      while (i < input.length) {
+
+          enc1 = this._keyStr.indexOf(input.charAt(i++));
+          enc2 = this._keyStr.indexOf(input.charAt(i++));
+          enc3 = this._keyStr.indexOf(input.charAt(i++));
+          enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+          chr1 = (enc1 << 2) | (enc2 >> 4);
+          chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+          chr3 = ((enc3 & 3) << 6) | enc4;
+
+          output = output + String.fromCharCode(chr1);
+
+          if (enc3 != 64) {
+              output = output + String.fromCharCode(chr2);
+          }
+          if (enc4 != 64) {
+              output = output + String.fromCharCode(chr3);
+          }
+
+      }
+
+      output = Base64._utf8_decode(output);
+
+      return output;
+
+  },
+
+  // private method for UTF-8 encoding
+  _utf8_encode : function (string) {
+      string = string.replace(/\r\n/g,"\n");
+      var utftext = "";
+
+      for (var n = 0; n < string.length; n++) {
+
+          var c = string.charCodeAt(n);
+
+          if (c < 128) {
+              utftext += String.fromCharCode(c);
+          }
+          else if((c > 127) && (c < 2048)) {
+              utftext += String.fromCharCode((c >> 6) | 192);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+          else {
+              utftext += String.fromCharCode((c >> 12) | 224);
+              utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+              utftext += String.fromCharCode((c & 63) | 128);
+          }
+
+      }
+
+      return utftext;
+  },
+
+  // private method for UTF-8 decoding
+  _utf8_decode : function (utftext) {
+      var string = "";
+      var i = 0;
+      var c = c1 = c2 = 0;
+
+      while ( i < utftext.length ) {
+
+          c = utftext.charCodeAt(i);
+
+          if (c < 128) {
+              string += String.fromCharCode(c);
+              i++;
+          }
+          else if((c > 191) && (c < 224)) {
+              c2 = utftext.charCodeAt(i+1);
+              string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+              i += 2;
+          }
+          else {
+              c2 = utftext.charCodeAt(i+1);
+              c3 = utftext.charCodeAt(i+2);
+              string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+              i += 3;
+          }
+
+      }
+
+      return string;
+  }
 }
 
 
--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 var global_volume = 0.4;
 
 var ad_context;
-var sound_source = ["sample.mp3"];
+var sound_source = [];//["sample.mp3"];
+for(var i=0; i< raw_data.length; i++){
+  sound_source.push("songs/"+raw_data[i].title);
+}
+
 var buffer_list_playable;
 var switch_buffer_player;
 var switch_gain_node;
@@ -12,6 +16,7 @@ var buffer_player;
 var gain_node;
 var gain_val;
 var timeouts = [];
+var total_song_checked = 0;
 
 var segments = [
   ["Seg 1",0],
@@ -35,7 +40,7 @@ function create_audio_context(){
     console.log("Audio context initiated");
     buffer_loader = new BufferLoader(ad_context,sound_source,buffer_loading_finished);
     buffer_loader.load();
-    console.log("Buffer loader finished loading");
+    console.log("Buffer loader started loading.. please wait");
   }
   catch(e) {
     alert('Web Audio API is not supported in this browser and no sound will be played');
@@ -45,8 +50,20 @@ function create_audio_context(){
 // trigger then buffer of all audio clips are loaded completely
 function buffer_loading_finished(bufferList) {
   console.log("Buffer loader compplete - loaded " + sound_source.length + " sounds");
+  setTimeout(function(){
+    // events after loading finished
+    msg("Welcome to the Calming Tech Music Store");
+    l("Click on the color strips to preview songs and use the check-boxes to select upto five songs that you want to purchase. Once you're done, click 'Go to the next step'.");
+    $("#next_step").show();
+
+  },500);
   buffer_list_playable = bufferList;
   build_ui();
+}
+
+// show message to user
+function msg(str){
+  $("#instruction").html(str);
 }
 
 // local play can play any buffer in the playlist at any time and cross fade into any other time in other buffer
@@ -56,7 +73,7 @@ function local_play(playlist,index,start_time){
       return;
     }
     switch_lock = true;
-    $(".segment").css({"cursor":"no-drop"});
+    $(".seg_part").css({"cursor":"no-drop"});
     gain_val = 0;
     switch_gain_val = global_volume;
     for (var i=0; i<timeouts.length; i++) {
@@ -72,16 +89,16 @@ function local_play(playlist,index,start_time){
           switch_gain_val = gain_val;
           switch_buffer_player = buffer_player;
           switch_gain_node = gain_node;
-          l("buffer player and gain node switched");
+          console.log("buffer player and gain node switched");
           switch_lock = false;
-          $(".segment").css({"cursor":"pointer"});
+          $(".seg_part").css({"cursor":"pointer"});
           return;
         }
         switch_gain_val -= 0.025;
         gain_val += 0.025;
         if(typeof switch_gain_node !== "undefined") switch_gain_node.gain.value = switch_gain_val;
         gain_node.gain.value = gain_val;
-        l(switch_gain_val+" , "+gain_val);
+        console.log(switch_gain_val+" , "+gain_val);
       },80 + 80 * i));
     }
     // fadein the new sound node
@@ -95,7 +112,7 @@ function local_play(playlist,index,start_time){
     buffer_player.start(0,start_time);
 }
 
-// set volume
+// set soung volume for local palyer
 function local_set_volume(val){
   gain_val = val;
   gain_node.gain.value = switch_gain_val;
@@ -103,46 +120,87 @@ function local_set_volume(val){
   switch_gain_node.gain.value = switch_gain_val;
 }
 
+// build bookmark ui
 function build_ui(){
   $("#music_seg").empty();
-  for(var i=0; i<segments.length; i++){
-    $("#music_seg").append(
-      ' <div class="segment" id="'+i+'">'+ segments[i][0]+'</div>'
-    );
+  for(var s=0; s<raw_data.length;s++){
+    var segs = raw_data[s].segment.split(" ");
+    var d = time_to_sec(raw_data[s].duration);
+    var seg_html = "<div class='seg_bar'><div class='sound_title hide'>"+raw_data[s].title.split(".")[0]+"</div>";
+    var pt = 0; // start time of a segment
+    var c_p = 0; // type of a segment
+    for(var i=0; i<segs.length; i++){
+      var c_t = time_to_sec(segs[i].split("_")[0])+1; // end time of a segment
+      if(i != 0){
+        c_p = segs[i-1].split("_")[1];
+      }
+      seg_html += "<div class='seg_part' id='"+s+"_"+pt+"' style='width:"+100*((c_t - pt)/d)+"%;background:"+sample_color_map[c_p]+"'></div>";
+      pt = c_t;
+    }
+    seg_html += "<div class='seg_part' id='"+s+"_"+pt+"' style='width:"+100*((d - pt)/d)+"%;background:"+sample_color_map[segs[segs.length-1].split("_")[1]]+"'></div>";
+    // append checkboxes
+    seg_html += '<input type="checkbox" class="ckbox" id="c_'+s+'" value="'+raw_data[s].title.split(".")[0]+'"><br>';
+    seg_html += "</div>";
+    $("#music_seg").append(seg_html);
   }
-  $(".segment").click(function(){
+  // attach check box event
+  $(".ckbox").click(function(){
+    if($(this).is(":checked")){
+      if(total_song_checked == 5){
+        alert("You can check at most 5 songs!");
+        $(this).prop('checked', false);
+        return;
+      }
+      total_song_checked += 1;
+      log_gen("checkbox select",this.id,$(this).attr("value"));
+    }else{
+      total_song_checked -= 1;
+      log_gen("checkbox unselect",this.id,$(this).attr("value"));
+    }
+    
+  });
+
+  // play music buffer when click
+  $(".seg_part").click(function(){
     if(switch_lock){
       return;
     }
-    $(".segment").removeClass("sel");
+    $(".seg_part").removeClass("sel");
     $(this).addClass("sel");
-    local_play(buffer_list_playable,0,segments[parseInt(this.id)][1]);
+    var id_info = this.id.split("_");
+    local_play(buffer_list_playable,id_info[0],id_info[1]);
   });
 }
 
-// logging
+// generate a log entry
+function log_gen(act,d){
+  log_data.push({
+    time:new Date(),
+    action:act,
+    data:d
+  });
+}
+
+// quick logging (also appear on the ui)
 function l(msg){
   console.log(msg)
   $("#status").html(msg);
 }
 
-function build_seg_bars(){
-  
-  for(var i=0;i<sample_seg_data.length;i++){
-    var segments = sample_seg_data[i].segments;
-    var d = sample_seg_data[i].duration;
-    var seg_bar = "<div class='seg_bar'><div class='sound_title'>Sound Title</div>";
-    var pt = 0;
-    for(var j=0; j<segments.length;j++ ){
-      seg_bar += "<div class='seg_part' style='width:"+100*((segments[j].time - pt)/d)+"%;background:"+sample_color_map[segments[j].type]+"'></div>";
-      pt = segments[j].time;
-    }
-    seg_bar += "</div>"
-    $("#seg_area").append(seg_bar);
-  }
-
+// player any buffer (for tempprary testing purpose only)
+function play_buffer(buffer){
+    if(typeof tmp_buffer_player!== "undefined")tmp_buffer_player.stop(0);
+    tmp_buffer_player = ad_context.createBufferSource();
+    tmp_buffer_player.buffer = buffer;
+    tmp_buffer_player.connect(ad_context.destination);
+    tmp_buffer_player.start(0);
 }
 
+// transform 4:30 to 4*60 + 30
+function time_to_sec(t){
+  var t_s = t.split(":");
+  return t_s[0]*60 + parseInt(t_s[1]);
+}
 
 // drag and drop functions for music
 $(document).ready(function(){
@@ -175,6 +233,7 @@ $(document).ready(function(){
           event.preventDefault();
           processFiles(event.dataTransfer.files);
           dropArea.className = 'uploading';
+          dropArea.innerHTML = "Loading..";
       }
 
       // process bunch of files
@@ -194,7 +253,10 @@ $(document).ready(function(){
                 console.log("Loaded buffer with length "+buffer.length);
                 drop_buffer_list_playable.push(buffer);
                 load_progress += 1;
-                if(load_progress == list.length) dropArea.className = '';
+                if(load_progress == list.length){
+                  dropArea.className = '';
+                  dropArea.innerHTML = "Drop Area";
+                }
               }, function(){alert("error loading!");} ); 
             };
             reader_list[b].onerror = function (event) {
@@ -208,21 +270,14 @@ $(document).ready(function(){
   })();
 });
 
-// buffer decode helper
-function play_buffer(buffer){
-    if(typeof tmp_buffer_player!== "undefined")tmp_buffer_player.stop(0);
-    tmp_buffer_player = ad_context.createBufferSource();
-    tmp_buffer_player.buffer = buffer;
-    tmp_buffer_player.connect(ad_context.destination);
-    tmp_buffer_player.start(0);
-}
-
-
+// loading and loading percentage
+var loading_progress = 0;
+var loading_progress_inc = 100/sound_source.length;
 // buffer loader class
 function BufferLoader(context,urlList,callback){this.context=context;this.urlList=urlList;this.onload=callback;this.bufferList=new Array();this.loadCount=0;}
 BufferLoader.prototype.loadBuffer=function(url,index){var request=new XMLHttpRequest();request.open("GET",url,true);request.responseType="arraybuffer";var loader=this;request.onload=function(){loader.context.decodeAudioData(request.response,function(buffer){if(!buffer){alert('error decoding file data: '+url);return;}
 loader.bufferList[index]=buffer;if(++loader.loadCount==loader.urlList.length)
-loader.onload(loader.bufferList);},function(error){console.error('decodeAudioData error',error);});}
+loader.onload(loader.bufferList);loading_progress+=loading_progress_inc; l("loading.. " + loading_progress.toFixed(2) + "%")},function(error){console.error('decodeAudioData error',error);});}
 request.onerror=function(){alert('BufferLoader: XHR error');}
 request.send();}
 BufferLoader.prototype.load=function(){for(var i=0;i<this.urlList.length;++i)
